6f9b90a2b99fba2bcbaa2adca36eee09b52e94a4
bigmasonwang <bigmasonwang@gmail.com>
2021-02-11 11:06:29 +1030

    error handling

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoute from './routes/productRoutes.js';
 
@@ -14,6 +15,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoute);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
