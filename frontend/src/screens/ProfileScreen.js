a1565dd9479170752628505fcba04652c3596891
bigmasonwang <bigmasonwang@gmail.com>
2021-02-14 18:58:47 +1030

    shipping screen state

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -28,7 +28,7 @@ const ProfileScreen = ({ location, history }) => {
     if (!userInfo) {
       history.push('/login');
     } else {
-      if (!user.name) {
+      if (!name || !user.name) {
         dispatch(getUserDetails('profile'));
       } else {
         setName(user.name);
